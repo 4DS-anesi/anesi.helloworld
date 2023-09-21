b95cc0ef3967cf819c6da2df671012a11d5ef217
christian.anesi <christian.anesi@buonarroti.tn.it>
2023-09-21 14:10:20 +0200

    feat: Classe che gestisce il saluto

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    private String saluto;
+
+    public HelloWorld(){
+        saluto="Hello world";
+    }
+
+    @Override
+    public String toString() {
+        return saluto;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HelloWorld oby=new HelloWorld();
+
+        System.out.println(oby.toString());
+
     }
 }
